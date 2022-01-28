67ba9ec32ffd53e0ab591d6fa3d44fd57f187e39
Sean Rahman <terturlminecraft@gmail.com>
2022-01-28 09:23:38 -0800

    Changed all Bukkit.getConsoleSender().sendMessage() to sendConsoleMessage() Added support for giving Drugs their SDRecipe if they have it

--- src/main/java/me/Coderforlife/SimpleDrugs/Druging/DrugManager.java
+++ src/main/java/me/Coderforlife/SimpleDrugs/Druging/DrugManager.java
@@ -14,11 +14,9 @@ import java.util.StringJoiner;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffectType;
 
@@ -32,6 +30,7 @@ import com.google.gson.JsonSyntaxException;
 import me.Coderforlife.SimpleDrugs.Main;
 import me.Coderforlife.SimpleDrugs.Crafting.CraftingComponent;
 import me.Coderforlife.SimpleDrugs.Crafting.DrugCraftingType;
+import me.Coderforlife.SimpleDrugs.Crafting.Recipes.SDRecipe;
 import me.Coderforlife.SimpleDrugs.Crafting.Recipes.SDShaped;
 import me.Coderforlife.SimpleDrugs.Crafting.Recipes.SDShapeless;
 import me.Coderforlife.SimpleDrugs.DrugPlants.DrugPlantItem;
@@ -64,7 +63,7 @@ public class DrugManager {
     	for(Drug d : getallDrugs()) {
     		enabled.add(d.getName());
     	}
-    	if(enabled.length() > 0) Bukkit.getConsoleSender().sendMessage("§6Enabled Drugs: §a" + enabled.toString().trim());
+    	if(enabled.length() > 0) sendConsoleMessage("§6Enabled Drugs: §a" + enabled.toString().trim());
     }
     
     private void loadDrugs() {
@@ -74,9 +73,9 @@ public class DrugManager {
 					JsonObject obj = new Gson().fromJson(new FileReader(f), JsonObject.class);
 					DrugLoadError dle = canDrugLoad(obj);
 					if(!dle.canLoad()) {
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + f.getName());
+						sendConsoleMessage("§c[ERROR] Error in: §7" + f.getName());
 						dle.printAllErrors();
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Drug");
+						sendConsoleMessage("§c[ERROR] Skipping Drug");
 						continue;
 					}
 					drugFromJson(obj, f.getName());
@@ -95,9 +94,9 @@ public class DrugManager {
 					obj = new Gson().fromJson(new FileReader(f), JsonObject.class);
 					DrugLoadError dle = canRecipeLoad(obj, true);
 					if(!dle.canLoad()) {
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + f.getName());
+						sendConsoleMessage("§c[ERROR] Error in: §7" + f.getName());
 						dle.printAllErrors();
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Seed Recipe");
+						sendConsoleMessage("§c[ERROR] Skipping Seed Recipe");
 						continue;
 					}
 					
@@ -106,18 +105,18 @@ public class DrugManager {
 					Drug d = config.getDrug("drug");
 					
 					if(d == null) {
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + f.getName());
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Drug not found: §7" + config.getString("drug"));
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Recipe");
+						sendConsoleMessage("§c[ERROR] Error in: §7" + f.getName());
+						sendConsoleMessage("§c[ERROR] Drug not found: §7" + config.getString("drug"));
+						sendConsoleMessage("§c[ERROR] Skipping Recipe");
 						continue;
 					}
 					
 					DrugCraftingType dct = config.contains("type") ? config.getDrugCraftingType("type") : DrugCraftingType.SHAPED;
 					JsonArray ja = config.getJsonArray("recipe");
 					if(dct.equals(DrugCraftingType.SHAPED) && ja.size() != 9) {
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + f.getName());
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Shaped Recipes require 9 items or air if none");
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Recipe");
+						sendConsoleMessage("§c[ERROR] Error in: §7" + f.getName());
+						sendConsoleMessage("§c[ERROR] Shaped Recipes require 9 items or air if none");
+						sendConsoleMessage("§c[ERROR] Skipping Recipe");
 						continue;
 					}
 					
@@ -125,20 +124,20 @@ public class DrugManager {
 					ItemStack seedItem = config.getItem("seed-item");
 					
 					if(config.getAllError().size() > 0) {
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + f.getName());
+						sendConsoleMessage("§c[ERROR] Error in: §7" + f.getName());
 			    		config.getAllError().forEach(e -> {
 			    			Bukkit.getConsoleSender().sendMessage(e);
 			    		});
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Drug");
+			    		sendConsoleMessage("§c[ERROR] Skipping Drug");
 						continue;
 					}
 					
 					DrugPlantItem dpi = new DrugPlantItem(d, seedItem, Material.FARMLAND, harvestAmount);
 					
 					if(!loadSeedCrafting(f.getName(), dpi, ja, dct)) {
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + f.getName());
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in Recipe");
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Recipe");
+						sendConsoleMessage("§c[ERROR] Error in: §7" + f.getName());
+						sendConsoleMessage("§c[ERROR] Error in Recipe");
+						sendConsoleMessage("§c[ERROR] Skipping Recipe");
 						continue;
 					}
 					
@@ -158,9 +157,9 @@ public class DrugManager {
 					JsonObject obj = new Gson().fromJson(new FileReader(f), JsonObject.class);
 					DrugLoadError dle = canRecipeLoad(obj, false);
 					if(!dle.canLoad()) {
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + f.getName());
+						sendConsoleMessage("§c[ERROR] Error in: §7" + f.getName());
 						dle.printAllErrors();
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Recipe");
+						sendConsoleMessage("§c[ERROR] Skipping Recipe");
 						continue;
 					}
 					
@@ -169,37 +168,40 @@ public class DrugManager {
 					Drug d = config.getDrug("drug");
 					
 					if(d == null) {
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + f.getName());
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Drug not found: §7" + config.getString("drug"));
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Recipe");
+						sendConsoleMessage("§c[ERROR] Error in: §7" + f.getName());
+						sendConsoleMessage("§c[ERROR] Drug not found: §7" + config.getString("drug"));
+						sendConsoleMessage("§c[ERROR] Skipping Recipe");
 						continue;
 					}
 					
 					DrugCraftingType dct = config.contains("type") ? config.getDrugCraftingType("type") : DrugCraftingType.SHAPED;
 					JsonArray ja = config.getJsonArray("recipe");
 					if(dct.equals(DrugCraftingType.SHAPED) && ja.size() != 9) {
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + f.getName());
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Shaped Recipes require 9 items or air if none");
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Recipe");
+						sendConsoleMessage("§c[ERROR] Error in: §7" + f.getName());
+						sendConsoleMessage("§c[ERROR] Shaped Recipes require 9 items or air if none");
+						sendConsoleMessage("§c[ERROR] Skipping Recipe");
 						continue;
 					}
 					
 					if(config.getAllError().size() > 0) {
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + f.getName());
+						sendConsoleMessage("§c[ERROR] Error in: §7" + f.getName());
 			    		config.getAllError().forEach(e -> {
-			    			Bukkit.getConsoleSender().sendMessage(e);
+			    			sendConsoleMessage(e);
 			    		});
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Drug");
+			    		sendConsoleMessage("§c[ERROR] Skipping Drug");
 						continue;
 					}
 					
-					if(!loadCraftingRecipe(f.getName(), d, ja, dct)) {
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + f.getName());
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in Recipe");
-						Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Recipe");
+					SDRecipe recipe = loadCraftingRecipe(f.getName(), d, ja, dct);
+					
+					if(recipe == null) {
+						sendConsoleMessage("§c[ERROR] Error in: §7" + f.getName());
+						sendConsoleMessage("§c[ERROR] Error in Recipe");
+						sendConsoleMessage("§c[ERROR] Skipping Recipe");
 						continue;
 					}
 					
+					d.setRecipe(recipe);
 					d.setCraftable(true);
 					
 				} catch (JsonSyntaxException | JsonIOException | FileNotFoundException e) {
@@ -281,16 +283,16 @@ public class DrugManager {
     	}
     }
     
-    private boolean loadCraftingRecipe(String fileName, Drug d, JsonArray ja, DrugCraftingType dct) {
+    private SDRecipe loadCraftingRecipe(String fileName, Drug d, JsonArray ja, DrugCraftingType dct) {
     	switch(dct) {
 		case FURNACE:
-			return false;
+			return null;
 		case SHAPED:
 			SDShaped shaped = new SDShaped(d.getName(), d.getItem());
 			List<ItemStack> mats = loadMaterialsForCrafting(fileName, ja);
 			
 			if(mats == null) {
-				return false;
+				return null;
 			}
 			
 			for(int i = 0; i < mats.size(); i++) {
@@ -299,13 +301,13 @@ public class DrugManager {
 
 			shaped.registerRecipe();
 			
-			return true;
+			return shaped;
 		case SHAPELESS:
 			SDShapeless shapeless = new SDShapeless(d.getName(), d.getItem());
 			List<ItemStack> materials = loadMaterialsForCrafting(fileName, ja);
 			
 			if(materials == null) {
-				return false;
+				return null;
 			}
 			
 			materials.forEach(e -> {
@@ -314,9 +316,9 @@ public class DrugManager {
 			
 			shapeless.registerRecipe();
 			
-			return true;
+			return shapeless;
 		default:
-			return false;
+			return null;
     	}
     }
     
@@ -328,9 +330,9 @@ public class DrugManager {
     		if(m == null) {
     			CraftingComponent cc = Main.plugin.getCraftingManager().getByName(ja.get(i).getAsString().toUpperCase());
     			if(cc == null) {
-    				Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + fileName);
-    				Bukkit.getConsoleSender().sendMessage("§c[ERROR] Material: §7" + ja.get(i).getAsString().toUpperCase() + " §cdoes not exist as a Minecraft Material or Custom Crafting Component");
-    				Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Recipe");
+    				sendConsoleMessage("§c[ERROR] Error in: §7" + fileName);
+    				sendConsoleMessage("§c[ERROR] Material: §7" + ja.get(i).getAsString().toUpperCase() + " §cdoes not exist as a Minecraft Material or Custom Crafting Component");
+    				sendConsoleMessage("§c[ERROR] Skipping Recipe");
     				return null;
     			}
     			
@@ -341,8 +343,8 @@ public class DrugManager {
     	}
 		
     	if(materials.size() > 9) {
-    		Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + fileName);
-			Bukkit.getConsoleSender().sendMessage("§c[ERROR] Materials added cannot be above 9");
+    		sendConsoleMessage("§c[ERROR] Error in: §7" + fileName);
+    		sendConsoleMessage("§c[ERROR] Materials added cannot be above 9");
     		return null;
     	}
     	
@@ -376,11 +378,11 @@ public class DrugManager {
     	ArrayList<DrugEffect> effects = (config.contains("effects") && config.isJsonArray("effects")) ? loadEffectsFromJson(config.getJsonArray("effects"), fileName) : new ArrayList<DrugEffect>();
     	
     	if(config.getAllError().size() > 0) {
-    		Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + fileName);
+    		sendConsoleMessage("§c[ERROR] Error in: §7" + fileName);
     		config.getAllError().forEach(e -> {
     			Bukkit.getConsoleSender().sendMessage(e);
     		});
-			Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Drug");
+    		sendConsoleMessage("§c[ERROR] Skipping Drug");
     		return;
     	}
     	
@@ -423,11 +425,11 @@ public class DrugManager {
 			int intensity = config.contains("intensity") ? config.getInteger("intensity") : 1;
 			
 			if(config.getAllError().size() > 0) {
-				Bukkit.getConsoleSender().sendMessage("§c[ERROR] Error in: §7" + fileName);
+				sendConsoleMessage("§c[ERROR] Error in: §7" + fileName);
 				config.getAllError().forEach(e2 -> {
 	    			Bukkit.getConsoleSender().sendMessage(e2);
 	    		});
-				Bukkit.getConsoleSender().sendMessage("§c[ERROR] Skipping Effect");
+				sendConsoleMessage("§c[ERROR] Skipping Effect");
 				return;
 			}
 			
@@ -472,135 +474,15 @@ public class DrugManager {
         return false;
     }
 
-    private void DrugfromJson(JsonObject drug, String name) {
-        /* Displayname */
-        String displayname = drug.get("displayname").getAsString().replace("&", "§");
-
-        /* Effects */
-        JsonArray effects = drug.getAsJsonArray("effects");
-        ArrayList<DrugEffect> effectsList = new ArrayList<>();
-        for(JsonElement effect : effects) {
-            JsonObject effectObject = effect.getAsJsonObject();
-            PotionEffectType type = getEffectfromJson(effectObject);
-            int duration = effectObject.get("time").getAsInt();
-            int amplifier = effectObject.get("intensity").getAsInt() - 1;
-            effectsList.add(new DrugEffect(type, duration, amplifier));
-        }
-
-        /* Get the Item Result for Crafting */
-        ItemStack result = new ItemStack(MaterialFromObject(drug, "item"));
-        ItemMeta meta = result.getItemMeta();
-        meta.setDisplayName(displayname);
-        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_ATTRIBUTES);
-        ArrayList<String> lore = new ArrayList<>();
-        lore.add("§a§lEffects:");
-        for(DrugEffect effect : effectsList) {
-            lore.add("§7- §6" + effect.getEffect().getName().toUpperCase(Locale.ROOT));
-        }
-        lore.add("§7Click to Use");
-        meta.setLore(lore);
-        result.setItemMeta(meta);
-        result.addUnsafeEnchantment(Enchantment.ARROW_FIRE, 1);
-
-        /* Generate the Recipe */
-        JsonArray recipe = drug.getAsJsonArray("recipe");
-        NamespacedKey key = new NamespacedKey(Main.plugin, "drugs_" + name);
-        ShapedRecipe shapedRecipe = new ShapedRecipe(key, result);
-
-        shapedRecipe.shape("ABC", "DEF", "GHI");
-        shapedRecipe.setIngredient('A', MaterialFromArray(recipe, 0));
-        shapedRecipe.setIngredient('B', MaterialFromArray(recipe, 1));
-        shapedRecipe.setIngredient('C', MaterialFromArray(recipe, 2));
-        shapedRecipe.setIngredient('D', MaterialFromArray(recipe, 3));
-        shapedRecipe.setIngredient('E', MaterialFromArray(recipe, 4));
-        shapedRecipe.setIngredient('F', MaterialFromArray(recipe, 5));
-        shapedRecipe.setIngredient('G', MaterialFromArray(recipe, 6));
-        shapedRecipe.setIngredient('H', MaterialFromArray(recipe, 7));
-        shapedRecipe.setIngredient('I', MaterialFromArray(recipe, 8));
-
-        boolean craftable = drug.get("crafting").getAsBoolean();
-        String permission = drug.get("permission").getAsString();
-        
-        if(craftable)
-            Main.plugin.getServer().addRecipe(shapedRecipe);
-
-        Drug d = new Drug(name, displayname, shapedRecipe, effectsList, result, permission, craftable);
-        Boolean hasSeed = drug.has("has_seed") ? drug.get("has_seed").getAsBoolean() : false;
-        d.setHasSeed(hasSeed);
-        
-        if(drug.has("seed_recipe")) {
-        	JsonArray seedRecipe = drug.getAsJsonArray("seed_recipe");
-            NamespacedKey seedKey = new NamespacedKey(Main.plugin, "drugseeds_" + name);
-            
-            String seedMat = drug.has("seed_item") ? drug.get("seed_item").getAsString() : "WHEAT_SEEDS";
-            Integer harvestAmount = drug.has("seed_harvest_amount") ? drug.get("seed_harvest_amount").getAsInt() : 1;
-            
-            DrugPlantItem dpi = new DrugPlantItem(d, new ItemStack(Material.valueOf(seedMat)), Material.FARMLAND, harvestAmount);
-            
-            ShapedRecipe seedShapedRecipe = new ShapedRecipe(seedKey, dpi.makeItem());
-            
-            seedShapedRecipe.shape("ABC", "DEF", "GHI");
-            seedShapedRecipe.setIngredient('A', MaterialFromArray(seedRecipe, 0));
-            seedShapedRecipe.setIngredient('B', MaterialFromArray(seedRecipe, 1));
-            seedShapedRecipe.setIngredient('C', MaterialFromArray(seedRecipe, 2));
-            seedShapedRecipe.setIngredient('D', MaterialFromArray(seedRecipe, 3));
-            seedShapedRecipe.setIngredient('E', MaterialFromArray(seedRecipe, 4));
-            seedShapedRecipe.setIngredient('F', MaterialFromArray(seedRecipe, 5));
-            seedShapedRecipe.setIngredient('G', MaterialFromArray(seedRecipe, 6));
-            seedShapedRecipe.setIngredient('H', MaterialFromArray(seedRecipe, 7));
-            seedShapedRecipe.setIngredient('I', MaterialFromArray(seedRecipe, 8));
-            Main.plugin.getServer().addRecipe(seedShapedRecipe);
-            
-            d.setHarvestAmount(harvestAmount);
-            d.setSeedItem(Material.valueOf(seedMat));
-            d.setHasSeed(hasSeed);
-            d.setSeedRecipe(seedShapedRecipe);
-        }
-        
-        addDrug(d, name);
-    }
-
     public void createDrugs() {
         sendConsoleMessage(ChatColor.BLUE + "[INFO] No Drugs where Found in your Folder. Creating Default Drugs!");
         Reader reader = new InputStreamReader(this.getClass().getResourceAsStream("/drugs.json"));
         JsonArray drugs = new Gson().fromJson(reader, JsonArray.class);
         for(JsonElement drug : drugs) {
-            DrugfromJson(drug.getAsJsonObject(), drug.getAsJsonObject().get("name").getAsString());
+            drugFromJson(drug.getAsJsonObject(), drug.getAsJsonObject().get("name").getAsString());
         }
+        // TODO: Save them
         sendConsoleMessage("§aDefault Drugs Created! Enjoy Simple-Drugs :D");
-    }
-
-    private PotionEffectType getEffectfromJson(JsonObject element) {
-        PotionEffectType effect = PotionEffectType.getByName(element.get("type").getAsString().toUpperCase());
-        if(effect != null)
-            return effect;
-
-        sendConsoleMessage("§c[ERROR] Could not find Effect: §7" + element.get("type").getAsString());
-        sendConsoleMessage("§c[ERROR] Make Sure the Name is Correct!");
-        sendConsoleMessage("§c[INFO] Replacing all Unknown Effects with POISON to Ensure the Plugin keeps Running!");
-        return PotionEffectType.POISON;
-    }
-
-    private Material MaterialFromArray(JsonArray obj, int i) {
-        Material mat = Material.getMaterial(obj.get(i).getAsString().toUpperCase());
-        if(mat != null)
-            return mat;
-
-        sendConsoleMessage("§c[ERROR] Could not find Material: §7" + obj.get(i).getAsString());
-        sendConsoleMessage("§c[ERROR] Make Sure the Name is Correct!");
-        sendConsoleMessage("§b[INFO] Replacing all Unknown Items with BONE to Ensure the Plugin keeps Running!");
-        return Material.BONE;
-    }
-
-    private Material MaterialFromObject(JsonObject obj, String key) {
-        Material mat = Material.getMaterial(obj.get(key).getAsString().toUpperCase());
-        if(mat != null)
-            return mat;
-
-        sendConsoleMessage("§c[ERROR] Could not find Material: §7" + obj.get(key).getAsString());
-        sendConsoleMessage("§cMake Sure the Name is Correct!");
-        sendConsoleMessage("§b[INFO] Replacing all Unknown Items with BONE to Ensure the Plugin keeps Running!");
-        return Material.BONE;
     }
 
     private void sendConsoleMessage(String message) {
