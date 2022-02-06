1325bfb634a3e2b9d78d596a260bd7e7fd8fec97
Sean Rahman <terturlminecraft@gmail.com>
2022-02-06 15:32:28 -0800

    Added a way to be able to view potion effects being added to a drug

---
+++ src/main/java/me/Coderforlife/SimpleDrugs/GUI/DrugCreatorTest/Util/InventoryPotionEffect.java
@@ -0,0 +1,29 @@
+package me.Coderforlife.SimpleDrugs.GUI.DrugCreatorTest.Util;
+
+import org.bukkit.potion.PotionEffectType;
+
+public class InventoryPotionEffect {
+
+	private PotionEffectType type;
+	private Integer time;
+	private Integer intensity;
+	
+	public InventoryPotionEffect(PotionEffectType pet, Integer t, Integer i) {
+		type = pet;
+		time = t;
+		intensity = i;
+	}
+	
+	public PotionEffectType getType() {
+		return type;
+	}
+	
+	public Integer getTime() {
+		return time;
+	}
+	
+	public Integer getIntensity() {
+		return intensity;
+	}
+	
+}
---
+++ src/main/java/me/Coderforlife/SimpleDrugs/GUI/DrugCreatorTest/Util/PotionEffectInventoryUtil.java
@@ -0,0 +1,14 @@
+package me.Coderforlife.SimpleDrugs.GUI.DrugCreatorTest.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PotionEffectInventoryUtil {
+
+	private List<InventoryPotionEffect> potionEffects = new ArrayList<>();
+	
+	public List<InventoryPotionEffect> getPotionEffects() {
+		return potionEffects;
+	}
+	
+}
