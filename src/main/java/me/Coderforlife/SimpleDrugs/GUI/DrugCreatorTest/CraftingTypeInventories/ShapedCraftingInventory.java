bc537df44a16ddc7d8a36b83f21bfe4a77a4c31f
Sean Rahman <terturlminecraft@gmail.com>
2022-02-06 17:44:57 -0800

    Updated ShapedInventory Editory
    Added the inventories to save the items in the crafting recipe to be used later as well as updated the inventory to add the AddictionLevel setting, setting/removing/viewing potion effects, and to add in support for AbstractClass handler

--- src/main/java/me/Coderforlife/SimpleDrugs/GUI/DrugCreatorTest/CraftingTypeInventories/ShapedCraftingInventory.java
+++ src/main/java/me/Coderforlife/SimpleDrugs/GUI/DrugCreatorTest/CraftingTypeInventories/ShapedCraftingInventory.java
@@ -1,36 +1,61 @@
 package me.Coderforlife.SimpleDrugs.GUI.DrugCreatorTest.CraftingTypeInventories;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import me.Coderforlife.SimpleDrugs.GUI.DrugCreatorTest.Util.InventoryPotionEffect;
+import me.Coderforlife.SimpleDrugs.GUI.DrugCreatorTest.Util.PotionEffectInventoryUtil;
+import me.Coderforlife.SimpleDrugs.GUI.DrugCreatorTest.Util.PotionEffectRemoveInventory;
+import me.Coderforlife.SimpleDrugs.GUI.DrugCreatorTest.Util.PotionEffectSelectorInventory;
+import me.Coderforlife.SimpleDrugs.GUI.DrugCreatorTest.Util.PotionEffectSetterInventory;
+import me.Coderforlife.SimpleDrugs.GUI.DrugCreatorTest.Util.SetAddictionInventory;
 import me.Coderforlife.SimpleDrugs.GUI.Framework.ClickAction;
 import me.Coderforlife.SimpleDrugs.GUI.Framework.InventoryButton;
-import me.Coderforlife.SimpleDrugs.GUI.Framework.InventoryUI;
 import net.md_5.bungee.api.ChatColor;
 
-public class ShapedCraftingInventory extends InventoryUI {
+public class ShapedCraftingInventory extends PotionEffectSetterInventory {
 
+	private PotionEffectInventoryUtil peiu;
+	private ShapedCraftingInventory sci;
+	private Map<Integer, ItemStack> items = new HashMap<>();
+	private double addLevel = 0.1;
+	
 	public ShapedCraftingInventory(String drugName) {
 		super(27, ChatColor.translateAlternateColorCodes('&', "&6&lCreate Shaped Recipe"));
+		setShouldRemove(false);
+		
+		peiu = new PotionEffectInventoryUtil();
+		sci = this;
 		
 		addAllNullButtons();
+		addEffectButton();
+		addAddictionButton();
 		
-		addButton(new InventoryButton(Material.PAPER, "&6&lPotion Effects", "") {
+		addButton(new InventoryButton(Material.BARRIER, "&4&lRemove Potion Effect", "") {
 			@Override
 			public void onPlayerClick(Player p, ClickAction action) {
-				
+				if(getPotionEffects().getPotionEffects().size() == 0) return;
+				saveItems();
+				close(p);
+				PotionEffectRemoveInventory peri = new PotionEffectRemoveInventory(sci);
+				peri.open(p);
 			}
-		}, 1);
+		}, 10);
 		
 		addButton(new InventoryButton(Material.POTION, "&6&lAdd Potion Effect", "") {
 			@Override
 			public void onPlayerClick(Player p, ClickAction action) {
-				
+				saveItems();
+				close(p);
+				PotionEffectSelectorInventory pesi = new PotionEffectSelectorInventory(sci);
+				pesi.open(p);
 			}
 		}, 19);
 		
@@ -41,7 +66,7 @@ public class ShapedCraftingInventory extends InventoryUI {
 			}
 		}, 8);
 		
-		addButton(new InventoryButton(Material.GREEN_WOOL, "&4&lAdd Drug", "") {
+		addButton(new InventoryButton(Material.GREEN_WOOL, "&2&lAdd Drug", "") {
 			@Override
 			public void onPlayerClick(Player p, ClickAction action) {
 				close(p);
@@ -51,13 +76,70 @@ public class ShapedCraftingInventory extends InventoryUI {
 		updateInventory();
 	}
 	
-	private void addEffectButton() {
+	private void saveItems() {
+		items.clear();
 		
+		for(int i = 4; i < 7; i++) {
+			if(getInventory().getItem(i) == null || getInventory().getItem(i).getType().equals(Material.AIR)) continue;
+			items.put(i, getInventory().getItem(i));
+		}
+		for(int i = 12; i < 15; i++) {
+			if(getInventory().getItem(i) == null || getInventory().getItem(i).getType().equals(Material.AIR)) continue;
+			items.put(i, getInventory().getItem(i));
+		}
+		for(int i = 21; i < 24; i++) {
+			if(getInventory().getItem(i) == null || getInventory().getItem(i).getType().equals(Material.AIR)) continue;
+			items.put(i, getInventory().getItem(i));
+		}
+	}
+	
+	private void reApplyItems() {
+		items.forEach((k, v) -> {
+			getInventory().setItem(k, v);
+		});
+	}
+	
+	private void addEffectButton() {
+		ItemStack item = new ItemStack(Material.PAPER);
+		ItemMeta im = item.getItemMeta();
+		im.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&6&lPotion Effects"));
+		List<String> lore = new ArrayList<>();
+		if(peiu.getPotionEffects().size() == 0) {
+			lore.add("No Effects");
+		} else {
+			for(InventoryPotionEffect ipe : peiu.getPotionEffects()) {
+				lore.add(ChatColor.translateAlternateColorCodes('&', "&b&o" + ipe.getType().getName().toLowerCase().replaceAll("_", " ") + " Time: " + "&c&o" + String.valueOf(ipe.getTime()) + " &b&oIntensity: " + "&c&o" + String.valueOf(ipe.getIntensity())));
+			}
+		}
+		im.setLore(lore);
+		item.setItemMeta(im);
+		
+		addButton(new InventoryButton(item) {
+			public void onPlayerClick(Player p, ClickAction action) { }
+		}, 1);
+	}
+	
+	private void addAddictionButton() {
+		ItemStack item = new ItemStack(Material.GUNPOWDER);
+		ItemMeta im = item.getItemMeta();
+		im.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&6&lSet Addiction Level"));
+		List<String> lore = new ArrayList<>();
+		lore.add(ChatColor.translateAlternateColorCodes('&', "&bCurrent Addiction Level: &c" + String.valueOf(addLevel)));
+		im.setLore(lore);
+		item.setItemMeta(im);
+		
+		addButton(new InventoryButton(item) {
+			public void onPlayerClick(Player p, ClickAction action) {
+				saveItems();
+				close(p);
+				SetAddictionInventory sai = new SetAddictionInventory(sci);
+				sai.open(p);
+			}
+		}, 9);
 	}
 	
 	private void addAllNullButtons() {
 		InventoryButton ib = new InventoryButton(Material.BLACK_STAINED_GLASS_PANE, " ", "") {
-			@Override
 			public void onPlayerClick(Player p, ClickAction action) {}
 		};
 		
@@ -65,7 +147,6 @@ public class ShapedCraftingInventory extends InventoryUI {
 		addButton(ib, 2);
 		addButton(ib, 6);
 		addButton(ib, 7);
-		addButton(ib, 9);
 		addButton(ib, 10);
 		addButton(ib, 11);
 		addButton(ib, 15);
@@ -89,6 +170,32 @@ public class ShapedCraftingInventory extends InventoryUI {
 		addButton(ib, 20);
 		addButton(ib, 24);
 		addButton(ib, 25);
+	}
+
+	@Override
+	public PotionEffectInventoryUtil getPotionEffects() {
+		return peiu;
+	}
+
+	@Override
+	public void updateEffectsButton() {
+		clearSlot(1);
+		reApplyItems();
+		addEffectButton();
+		updateInventory();
+	}
+	
+	@Override
+	public void updateAddictionButton() {
+		clearSlot(9);
+		reApplyItems();
+		addAddictionButton();
+		updateInventory();
+	}
+
+	@Override
+	public void setAddLevel(double b) {
+		addLevel = b;
 	}
 	
 }
